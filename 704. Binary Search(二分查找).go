ccc90fd57a8af070419a4f56f198c3925243259a
denganliang <denganliang@vip.qq.com>
2021-07-11 19:06:19 +0800

    Update 704. Binary Search(二分查找).go
    improve

--- 704. Binary Search(二分查找).go
+++ 704. Binary Search(二分查找).go
@@ -5,16 +5,11 @@ func BiarySearch(nums []int, target int, low int, high int) int {
     mid := low + (high - low)/2
     if nums[mid] == target {
         return mid
+    } else if nums[mid] < target {
+        return BiarySearch(nums, target, mid+1, high)
+    } else {
+        return BiarySearch(nums, target, low, mid-1)
     }
-    left := BiarySearch(nums, target, low, mid-1)
-    if left != -1 {
-        return left
-    }
-    right := BiarySearch(nums, target, mid+1, high)
-    if right != -1 {
-        return right
-    }
-    return -1
 }
 func search(nums []int, target int) int {
     return BiarySearch(nums, target, 0, len(nums)-1)
